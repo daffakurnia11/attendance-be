c692681e437208742e41068fe9d6585354dcca4e
Daffa Kurnia Fatah <daffakurniaf11@gmail.com>
2024-10-12 18:20:24 +0700

    feat(middleware): :sparkles: Create authenticate middleware

--- src/validators/AuthValidator.ts
+++ src/validators/AuthValidator.ts
@@ -3,7 +3,11 @@ import { body } from "express-validator";
 class AuthValidator {
   public register = [
     body("name").exists().withMessage("Name is required"),
-    body("email").exists().withMessage("Email is required"),
+    body("email")
+      .exists()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Invalid email"),
     body("password")
       .exists()
       .withMessage("Password is required")
@@ -21,13 +25,17 @@ class AuthValidator {
   ];
 
   public login = [
-    body("email").exists().withMessage("Email is required"),
+    body("email")
+      .exists()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Invalid email"),
     body("password")
       .exists()
       .withMessage("Password is required")
       .isLength({ min: 6 })
       .withMessage("Password must be at least 6 characters long"),
-  ]
+  ];
 }
 
 export default new AuthValidator();
